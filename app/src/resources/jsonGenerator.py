d6c1d26d7da544bb7a281a584ee89981756e19ff
Devon Gates <devonjgates@gmail.com>
2021-06-25 11:47:25 -0500

    Updated dictionary to include names and states

--- app/src/resources/jsonGenerator.py
+++ app/src/resources/jsonGenerator.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
 
+import re
+
 numberDict = {
-	0: [],
-	1: [],
-	2: ['A', 'B', 'C'],
-	3: ['D', 'E', 'F'],
-	4: ['G', 'H', 'I'],
-	5: ['J', 'K', 'L'],
-	6: ['M', 'N', 'O'],
-	7: ['P', 'Q', 'R', 'S'],
-	8: ['T', 'U', 'V'],
-	9: ['W', 'X', 'Y', 'Z'],
+    0: [],
+    1: [],
+    2: ['A', 'B', 'C', 'a', 'b', 'c'],
+    3: ['D', 'E', 'F', 'd', 'e', 'f'],
+    4: ['G', 'H', 'I', 'g', 'h', 'i'],
+    5: ['J', 'K', 'L', 'j', 'k', 'l'],
+    6: ['M', 'N', 'O', 'm', 'n', 'o'],
+    7: ['P', 'Q', 'R', 'S', 'p', 'q', 'r', 's'],
+    8: ['T', 'U', 'V', 't', 'u', 'v'],
+    9: ['W', 'X', 'Y', 'Z', 'w', 'x', 'y', 'z'],
 }
 
 letterDict = {}
-for k,v in numberDict.items():
+for k, v in numberDict.items():
     for x in v:
-        letterDict.setdefault(x,k)
+        letterDict.setdefault(x, k)
 
 parsedWords = []
 wordToNumberDict = {}
 
+pattern = re.compile('[\W_]+')
+
 with open('words.txt', 'r') as readFile:
     words = readFile.read()
-    parsedWords = [word.rstrip() for word in words.split('\n')][0:-1]
+    parsedWords = [pattern.sub('', word) for word in words.split('\n')][0:-1]
     for word in parsedWords:
         number = ''
         for letter in word:
             number += str(letterDict[letter])
         wordToNumberDict[word] = number
 
-numberToWordDict = {v:k for k, v in wordToNumberDict.items()}
+numberToWordDict = {v: k for k, v in wordToNumberDict.items()}
 
 with open('numberToWord.js', 'w') as writeFile:
-    writeFile.write(("{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in numberToWordDict.items()) + "}"))
+    writeFile.write('const numberToWord = ')
+    writeFile.write(("{" + "\n".join("{!r}: {!r},".format(k, v)
+                    for k, v in numberToWordDict.items()) + "}"))
+    writeFile.write('export default numberToWord')
